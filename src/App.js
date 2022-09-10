6368d954ceee6012450e7ab51f21240168a1d793
lauren-jones <105777927+lauren-jones@users.noreply.github.com>
2022-09-10 10:56:41 +0100

    Installed bootstrap

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
