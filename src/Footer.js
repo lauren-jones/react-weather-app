f97ebd03a025524e955400a220588c1ba3d3fa5e
lauren-jones <105777927+lauren-jones@users.noreply.github.com>
2022-09-10 16:25:07 +0100

    Updated footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,10 +4,7 @@ export default function Footer() {
   return (
     <div className="Footer">
       This project was coded by Lauren Jones and is{" "}
-      <a
-        href="https://github.com/lauren-jones/react-weather-app"
-        target="_blank"
-      >
+      <a href="https://github.com/lauren-jones/react-weather-app">
         open-sourced on GitHub
       </a>
     </div>
